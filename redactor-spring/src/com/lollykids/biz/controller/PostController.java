c3aa8c955ef328cd376ef24de269c50022f21a95
LEE HAK DO <future.fringe@gmail.com>
2013-11-04 09:41:10 +0900

    Signed-off-by: LEE HAK DO <future.fringe@gmail.com>

--- redactor-spring/src/com/lollykids/biz/controller/PostController.java
+++ redactor-spring/src/com/lollykids/biz/controller/PostController.java
@@ -1,16 +1,12 @@
 package com.lollykids.biz.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.lollykids.biz.mapper.UserMappr;
 import com.lollykids.biz.model.Mail;
-import com.lollykids.biz.model.User;
 import com.lollykids.biz.service.MailService;
 
 @Controller
